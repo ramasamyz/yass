a3eb96b619186a68f1c5c239905c7ace3960a800
Ramasamyz <ramasamy.zephyr@gmail.com>
2012-03-27 23:07:33 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yass::Application.config.secret_token = '0eb3620a7b907e212311d8b574de404ac0a73533c9e02d897b09de6048bbbf5eeb514799242fd33aa402a0cdc5350e5e636459fb0f51bb48923172b1569cf0b0'
